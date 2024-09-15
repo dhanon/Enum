c817c55b820019eb658bb9468b727a553f3e50fa
“dhanon” <dhanon68@gmail.com>
2024-09-15 23:44:28 +0600

    secode commit

--- Contents.swift
+++ Contents.swift
@@ -1,14 +1,62 @@
 import UIKit
 
-enum Phones: String{
-    case iPhone     = "I used this one."
-    case Samsung    = "I won't to switch."
-    case Pixel      = "I don't like Android"
-    case Nokia      = "Best phone ever."
+//Enum without rawValue
+enum Phones {
+    case iPhone
+    case Samsung
+    case Pixel
+    case Mi
 }
 
 func getOpinion(label phone: Phones) {
+    if phone == .iPhone {
+        print("I used this one.")
+    } else if phone == .Samsung{
+        print("I won't to switch.")
+    } else if phone == .Pixel {
+        print("I don't like Android.")
+    } else {
+        print("Best phone for Bangladesh.")
+    }
+}
+
+getOpinion(label: .Mi)
+
+//Enum with rawValue
+enum Phone: String {
+    case iPhone12     = "I used this one."
+    case SamsungS12   = "I won't to switch."
+    case Pixel19      = "I don't like Android."
+    case Nokia        = "Best phone ever."
+}
+
+func getMyOpinion(label phone: Phone) {
     print(phone.rawValue)
 }
 
-getOpinion(label: .Samsung)
+getMyOpinion(label: .Nokia)
+
+
+//Enum with Switch
+enum Phone2 {
+    case iPhone13
+    case SamsungNote
+    case GooglePixel
+    case MiNote
+}
+
+func chooseYourPhone(on phone2: Phone2) {
+    
+    switch phone2{
+    case .iPhone13:
+        print("I used this one.")
+    case .SamsungNote:
+        print("I won't to switch.")
+    case .GooglePixel:
+        print("I don't like Android.")
+    case .MiNote:
+        print("Best phone for Bangladesh.")
+    }
+}
+
+chooseYourPhone(on:.GooglePixel)
